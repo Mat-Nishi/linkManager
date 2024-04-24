8d195e303a26f9e8d50211fd73ec74001efde826
Mateus Nishimura <mateusnishimura@gmail.com>
2024-04-24 11:30:15 -0300

    Changed light mode styles

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,15 @@
 const button = document.getElementById("lightmode_switch");
 body = document.querySelector("html");
+profileImg = document.getElementById("profileImg")
 button.addEventListener("click", (e)=>{
     if (body.classList.contains("light")){
         body.classList.remove("light");
+        profileImg.src = "./styles/assets/avatar.png"
+        profileImg.alt = "Portrait of Mateus Nishimura wearing a white shirt in a dark background"
     }
     else{
         body.classList.add("light");
+        profileImg.src = "./styles/assets/avatar-light.png"
+        profileImg.alt = "Portrait of Mateus Nishimura wearing a white shirt in a light background"
     }
 });
